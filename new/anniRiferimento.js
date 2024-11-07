120b66c7066390625bd41e0fe8655b6d73140119
Alessio <61842431+alessi90s@users.noreply.github.com>
2024-11-07 15:02:24 +0100

    Update and rename years.js to anniRiferimento.js

---
+++ new/anniRiferimento.js
@@ -0,0 +1,18 @@
+const anniRiferimento = {
+    "2024": {
+        "gestioneSeparata": 26.07,
+        "inpsCommercianti": {
+            "contributoFisso": 4515.43,
+            "aliquotaVariabile": 24.48,
+            "sogliaReddito": 18415
+        },
+        "inpsArtigiani": {
+            "contributoFisso": 4427.04,
+            "aliquotaVariabile": 24,
+            "sogliaReddito": 18415
+        }
+    },
+    "2025": {
+        // Update percentages for 2025 as needed
+    }
+};
