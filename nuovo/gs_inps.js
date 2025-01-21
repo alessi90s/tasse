42849024a13c77a75537416bea0c95525f0c8728
Alessio <61842431+alessi90s@users.noreply.github.com>
2025-01-21 16:34:50 +0100

    Update gs_inps.js

--- nuovo/gs_inps.js
+++ nuovo/gs_inps.js
@@ -1,27 +1,24 @@
 // gs_inps.js
-
 export function calcolaContributi({ redditoNetto, annoReddito }) {
-    // Definizione delle percentuali per l'anno di riferimento
-    const aliquoteContributi = {
-        2023: 25.00, // 25% per il 2023
-        2024: 26.23, // 26.23% per il 2024
-        2025: 26.50, // Esempio: 26.50% per il 2025
-        // Aggiungi altri anni e percentuali se necessario
-    };
+  // Definizione delle percentuali per l'anno di riferimento
+  const aliquoteContributi = {
+    2023: 25.00, // 25% per il 2023
+    2024: 26.23, // 26.23% per il 2024
+    2025: 26.50  // 26.50% per il 2025
+  };
 
-    // Se l'anno non è specificato, usa la percentuale di default
-    const aliquotaGsInps = aliquoteContributi[annoReddito] ? aliquoteContributi[annoReddito] / 100 : 26.23 / 100;
+  const aliquotaGsInps = aliquoteContributi[annoReddito] ? aliquoteContributi[annoReddito] / 100 : 26.23 / 100;
 
-    const contributiInps = Math.round(redditoNetto * aliquotaGsInps);
-    const accontoInps = Math.round(contributiInps * 0.80);
+  const contributiInps = Math.round(redditoNetto * aliquotaGsInps);
+  const accontoInps = Math.round(contributiInps * 0.80);
 
-    return {
-        contributiFissi: 0,
-        contributiVariabili: 0,
-        contributiInps,
-        accontoInps,
-        contributiFissiIpotetici: 0,
-        contributiFissiSulAnno: contributiInps,
-        importiRate: {}
-    };
+  return {
+    contributiFissi: 0,
+    contributiVariabili: 0,
+    contributiInps,
+    accontoInps,
+    contributiFissiIpotetici: 0,
+    contributiFissiSulAnno: contributiInps,
+    importiRate: {}
+  };
 }
